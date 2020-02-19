ec30be69c64b2e9d82d82fabaeeba84b819965df
Alex Song <songya@umich.edu>
2020-02-18 19:41:52 -0500

    changes for eecs494 gold submit

--- bin/submit.sh
+++ bin/submit.sh
@@ -6,6 +6,8 @@ cp -r Assets/ Submit/Assets
 cp -r ProjectSettings/ Submit/ProjectSettings
 cp -r Builds/* Submit/
 cp -r GOLD_SPIKE.txt Submit/GOLD_SPIKE.txt
+cp -r PORTFOLIO.txt Submit/PORTFOLIO.txt
+cp -r ITERATION.txt Submit/ITERATION.txt
 
 
 zip -r songya_swarm_queen.zip Submit
